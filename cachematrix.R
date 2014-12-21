74ef1888422196f84b6ea4185a23cdd1964c642f
Contrastat <contrastat@gmail.com>
2014-12-21 15:29:55 +0100

    Modificacion de un error en CacheSolve
    Daba error porque faltaba un parentesis

--- cachematrix.R
+++ cachematrix.R
@@ -30,7 +30,7 @@ cacheSolve <- function(x, ...) {
         m <- x$getinverse()
         if(!is.null(m)){
           message("getting cached data")
-          return m
+          return(m)
         }
         data <- x$get()
         m <- solve(data, ...)
